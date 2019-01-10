1067060140fac1690adcbd96139830b924e91ad3
ceaserwayne <maxjing1025@gmail.com>
2019-01-10 17:11:08 +0800

    till -- datetime_filter

--- www/test_orm.py
+++ www/test_orm.py
@@ -4,13 +4,13 @@ from models import User, Blog, Comment
 async def test(loop):
     await orm.create_pool(loop,user='csw', password='csw', db='awesome')
 
-    u = User(name='CeaserWayne', email='maxjing1025@gmail.com', passwd='csw', image='about:blank')
+    u = User(name='Jason', email='maxjAson025@gmail.com', passwd='jason', image='about:blank')
 
     await u.save()
 
 def run(task=None):
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(task(loop,**kw) or test(loop))
+    loop.run_until_complete(test(loop))
 
 
 if __name__ == '__main__':
