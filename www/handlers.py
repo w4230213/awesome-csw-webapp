78aadff33e13defa9c7169b49b74af0ea2bdb813
ceaserwayne <maxjing1025@gmail.com>
2019-01-11 17:57:08 +0800

    add user manage feature

--- www/handlers.py
+++ www/handlers.py
@@ -1,19 +1,21 @@
-import time,re,json,hashlib,base64,asyncio
-import logging;logging.basicConfig(level=logging.INFO)
+import time, re, json, hashlib, base64, asyncio
+import logging;
+
+logging.basicConfig(level=logging.INFO)
 
 from coroweb import get, post
 from models import User, Comment, Blog, next_id
 
 from aiohttp import web
-from apis import APIValueError, APIResourceNotFoundError,APIError
+from apis import APIValueError, APIResourceNotFoundError, APIError
 from config import configs
 
 COOKIE_NAME = 'awesession'
 _COOKIE_KEY = configs.session.secret
 
-
 _RE_EMAIL = re.compile(r'^[a-z0-9\.\-\_]+\@[a-z0-9\-\_]+(\.[a-z0-9\-\_]+){1,4}$')
 _RE_SHA1 = re.compile(r'^[0-9a-f]{40}$')
+
 
 def user2cookie(user, max_age):
     '''
@@ -24,8 +26,8 @@ def user2cookie(user, max_age):
     :return:
     '''
 
-    expires = str(int(time.time()+max_age))
-    s = '%s-%s-%s-%s' % (user.id,user.passwd,expires,_COOKIE_KEY)
+    expires = str(int(time.time() + max_age))
+    s = '%s-%s-%s-%s' % (user.id, user.passwd, expires, _COOKIE_KEY)
     L = [user.id, expires, hashlib.sha1(s.encode('utf-8')).hexdigest()]
     return '-'.join(L)
 
@@ -65,9 +67,9 @@ async def cookie2user(cookie_str):
 def index(request):
     summary = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.'
     blogs = [
-        Blog(id='1', name='Test Blog', summary=summary, created_at=time.time()-120),
-        Blog(id='2', name='Something New', summary=summary, created_at=time.time()-3600),
-        Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time()-7200)
+        Blog(id='1', name='Test Blog', summary=summary, created_at=time.time() - 120),
+        Blog(id='2', name='Something New', summary=summary, created_at=time.time() - 3600),
+        Blog(id='3', name='Learn Swift', summary=summary, created_at=time.time() - 7200)
     ]
     return {
         '__template__': 'blogs.html',
@@ -78,8 +80,9 @@ def index(request):
 @get('/register')
 def register():
     return {
-        '__template__':'register.html'
+        '__template__': 'register.html'
     }
+
 
 @get('/signin')
 def signin():
@@ -89,26 +92,26 @@ def signin():
 
 
 @post('/api/authenticate')
-async def authenticate(*,email,passwd):
+async def authenticate(*, email, passwd):
     if not email:
         raise APIValueError('email', 'Invalid email.')
     if not passwd:
-        raise APIValueError('passwd','Empty password.')
-    users = await User.findAll('email=?',[email])
+        raise APIValueError('passwd', 'Empty password.')
+    users = await User.findAll('email=?', [email])
     if len(users) == 0:
-        raise APIValueError('email','Email not exist.')
+        raise APIValueError('email', 'Email not exist.')
     user = users[0]
     sha1 = hashlib.sha1()
     sha1.update(user.id.encode('utf-8'))
     sha1.update(b':')
     sha1.update(passwd.encode('utf-8'))
     if user.passwd != sha1.hexdigest():
-        raise APIValueError('passwd','Invalid password.')
+        raise APIValueError('passwd', 'Invalid password.')
     r = web.Response()
-    r.set_cookie(COOKIE_NAME,user2cookie(user,86400),max_age=86400,httponly='True')
+    r.set_cookie(COOKIE_NAME, user2cookie(user, 86400), max_age=86400, httponly='True')
     user.passwd = '******'
     r.content_type = 'application/json'
-    r.body = json.dumps(user,ensure_ascii=False).encode('utf-8')
+    r.body = json.dumps(user, ensure_ascii=False).encode('utf-8')
     return r
 
 
@@ -116,10 +119,9 @@ async def authenticate(*,email,passwd):
 async def signout(request):
     referer = request.header.get('Referer')
     r = web.HTTPFound(referer or '/')
-    r.set_cookie(COOKIE_NAME,'-deleted-',max_age=0,httponly='True')
+    r.set_cookie(COOKIE_NAME, '-deleted-', max_age=0, httponly='True')
     logging.info('user signed out.')
     return r
-
 
 
 @get('/api/users')
@@ -131,25 +133,25 @@ async def api_get_users():
 
 
 @post('/api/users')
-async def api_register_user(*,email,name,passwd):
+async def api_register_user(*, email, name, passwd):
     if not name or not name.strip():
         raise APIValueError('name')
     if not email or not _RE_EMAIL.match(email):
         raise APIValueError('email')
     if not passwd or not _RE_SHA1.match(passwd):
         raise APIValueError('passwd')
-    users = await User.findAll('email=?',[email])
-    if len(users)>0:
-        raise APIError('register:failed','email','Email is already in use')
+    users = await User.findAll('email=?', [email])
+    if len(users) > 0:
+        raise APIError('register:failed', 'email', 'Email is already in use')
     uid = next_id()
-    sha1_passwd = ('%s:%s' % (uid,passwd))
-    user = User(id=uid,name=name.strip(),email=email,
+    sha1_passwd = ('%s:%s' % (uid, passwd))
+    user = User(id=uid, name=name.strip(), email=email,
                 passwd=hashlib.sha1(sha1_passwd.encode('utf-8')).hexdigest(),
                 image='http://www.gravatar.com/avatar/%s?d=mm&s=120' % hashlib.md5(email.encode('utf-8')).hexdigest())
     await user.save()
     r = web.Response()
-    r.set_cookie(COOKIE_NAME, user2cookie(user,86400),max_age=86400,httponly='True')
+    r.set_cookie(COOKIE_NAME, user2cookie(user, 86400), max_age=86400, httponly='True')
     user.passwd = '******'
     r.content_type = 'application/json'
-    r.body = json.dumps(user,ensure_ascii=False).encode('utf-8')
+    r.body = json.dumps(user, ensure_ascii=False).encode('utf-8')
     return r
